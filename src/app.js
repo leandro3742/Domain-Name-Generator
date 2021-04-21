353b42d952cd963498c36d6db2c3ff32fec27301
leandro3742 <leandro.marrero@outlook.com>
2021-04-21 14:42:41 +0000

    ejercicio completo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let final = [".com", ".uy", ".ar", ".mx", ".net"];
+  let p = document.getElementById("cambiar");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < final.length; l++) {
+          let dominio = pronoun[i] + adj[j] + noun[k] + final[l];
+          p.innerHTML = p.innerHTML + `<br/>` + dominio;
+        }
+      }
+    }
+  }
 };
